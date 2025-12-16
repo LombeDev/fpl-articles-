7966963fa1ffdc47d4ebb82732d41d0ec2e7560b
LombeDev <81247194+LombeDev@users.noreply.github.com>
2025-12-16 14:39:38 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1314,17 +1314,4 @@ async function loadFDRTicker() {
 }
 
 
-// Old way: const proxy = "https://cors-anywhere.herokuapp.com/";
-// New way: Use your local redirect path
-const fplApiBase = "/api/"; 
 
-async function loadFDRTicker() {
-    // ... setup code ...
-    try {
-        const [fixturesRes, bootstrapRes] = await Promise.all([
-            fetch(`${fplApiBase}fixtures/`),
-            fetch(`${fplApiBase}bootstrap-static/`)
-        ]);
-        // ... rest of the code ...
-    }
-}
